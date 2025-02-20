5be94a5dd3efdf8b755cbabc5c893992e245abb3
Shourjo Chakravarty <cvtsh248@gmail.com>
2025-02-20 19:52:56 +0800

    made some changes

--- src/main.py
+++ src/main.py
@@ -3,11 +3,11 @@ from lib import pyMeSHsearch
 import asyncio
 import xml.etree.ElementTree as ET
 
-
+def search():
+    pass
 async def main():
     # await pyMeSHsearch.find_mesh()
     papers = await pyMeSHsearch.lit_search('''"Carcinoma, Renal Cell"[MeSH]''')
-    # literature = await pyMeSHsearch.find_paper(papers["esearchresult"]["idlist"][1])
     literature = await asyncio.gather(*(pyMeSHsearch.find_paper(id) for id in papers["esearchresult"]["idlist"]))
     filtered_literature = []
     filtered_ids = []
@@ -17,7 +17,7 @@ async def main():
             filtered_literature.append(item)
             filtered_ids.append(papers["esearchresult"]["idlist"][count])
     # literature = [item for item in literature if '[Error] : No result can be found.' not in item]
-    # print(len(literature))
+
     documents = []
     for paper in filtered_literature:
         print(paper)
@@ -35,10 +35,11 @@ async def main():
     )
 
     results = collection.query(
-        query_texts=["Which drugs are used in the treatment of renal cell carcinoma"], # Chroma will embed this for you
+        query_texts=["What is the cause of renal cell carcinoma"], # Chroma will embed this for you
         n_results=len(filtered_ids) # how many results to return
     )
     print(results["distances"])
+    print(results["ids"])
     print(len(literature))
 
 asyncio.run(main())
