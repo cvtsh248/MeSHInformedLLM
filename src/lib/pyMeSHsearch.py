5cc91839efedf0984cffa86a44b6ce5a73ab39ae
Shourjo Chakravarty <cvtsh248@gmail.com>
2025-02-22 11:05:15 +0800

    llm features

--- src/lib/pyMeSHsearch.py
+++ src/lib/pyMeSHsearch.py
@@ -15,7 +15,7 @@ async def find_MeSH(phrases: list[str]) -> list[dict]:
     labels = [item.replace(' ','%20') for item in phrases]
 
     # Batch search, idk why i dind't implement this elsewhere
-    results = await asyncio.gather(*(requestwrap.get("https://id.nlm.nih.gov/mesh/lookup/descriptor?label="+label+"&match=contains&year=current&limit=30", headers={"accept":"application/json"}) for label in labels))
+    results = await asyncio.gather(*(requestwrap.get("https://id.nlm.nih.gov/mesh/lookup/descriptor?label="+label+"&match=contains&year=current&limit=10", headers={"accept":"application/json"}) for label in labels))
     results = [result.json() for result in results]
     return results
 
@@ -34,6 +34,7 @@ async def find_paper(id: int) -> dict:
     return result.text
 
 async def MeSH_refiner(query: str) -> tuple[int, str]:
+    # print(query)
     
     '''
     Returns a response to the LLM after checking its' output if it needs to regenerate it. In this situation, a tuple with 1 in index 0 will be returned.
@@ -42,33 +43,58 @@ async def MeSH_refiner(query: str) -> tuple[int, str]:
     '''
 
     keyword_chars = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm,1234567890"
-    keywords = re.findall(r'"([^"]*)"', query)
     mesh = re.findall(r"\[([^\]]*)\]", query)
-
-    for i in mesh:
-        if i != "Mesh" or i != "MeSH":
-            return (1,"Please regenerate the query. You are missing the [MeSH] in your query")
-        
-    if len(mesh) == 0:
-        return (1,"Please regenerate the query. You are missing the [MeSH] in your query")
     
+
     refined_query = query
-    refined_query.replace("Mesh", "MeSH")
+    refined_query = refined_query.replace("Mesh", "MeSH")
+    refined_query = refined_query.replace("mesh", "MeSH")
+    refined_query = refined_query.replace('''"Mesh"''', "MeSH")
+    refined_query = refined_query.replace('''"MeSH"''', "MeSH")
+    refined_query = re.sub(r'"\[', '" [', refined_query)
+
+    print([x.lower() for x in mesh])
+    if "mesh" not in [x.lower() for x in mesh] and '''"mesh"''' not in [x.lower() for x in mesh]:
+        return (1,"Please regenerate the query. You are missing the [MeSH] in your query")
+        
+    # if len(mesh) == 0:
+    #     return (1,"Please regenerate the query. You are missing the [MeSH] in your query")
+    
+
+    keywords = re.findall(r'"([^"]*)"', query)
+    print(keywords)
+    if len(keywords) == 0:
+        return (1,"Please regenerate the query. You are missing the keywords in your query")
 
     # Check if the keywords the llama is thinking of exist
-    confirmed_keywords = await find_MeSH(keywords)
+    flipped_keywords = [" ".join(reversed(x.split(" "))) for x in keywords] # because the nih api is stupid and for some dumb reason is dependent on word order...
+    seperated_words = [x for y in keywords for x in y.split(" ")] # maximise search radius
 
-    confirmed_keywords = [keyword['label'] for keyword in confirmed_keywords[0]]
+    confirmed_keywords = await find_MeSH(keywords) + await find_MeSH(flipped_keywords) + await find_MeSH(seperated_words)
 
+    confirmed_keywords = [item for item in confirmed_keywords if item != []]
+
+    confirmed_keywords = [item for subitem in confirmed_keywords for item in subitem] # flatten dimensinos
+
+    # print(confirmed_keywords)
+
+    if len(confirmed_keywords) < len(keywords):
+        return (1, "Please regenerate the query. Not a single match was found for one of the terms used.")
+
+    confirmed_keywords = [keyword['label'] for keyword in confirmed_keywords]
+
+    # print(confirmed_keywords)
     for keyword in keywords:
         if keyword in confirmed_keywords:
             return (0,"") 
 
     # Find the closest real keywords to hallucinated keywords using cosine similarity
-    vectorised_keywords = embedding_functions.DefaultEmbeddingFunction(keywords)
+    embed = embedding_functions.DefaultEmbeddingFunction()
 
-    vectorised_confirmed_keywords = embedding_functions.DefaultEmbeddingFunction(confirmed_keywords)
-    
+    vectorised_keywords = embed(keywords)
+
+    vectorised_confirmed_keywords = embed(confirmed_keywords)
+
     distances: list[list] = []
     for i in vectorised_keywords:
         buffer = []
@@ -77,9 +103,15 @@ async def MeSH_refiner(query: str) -> tuple[int, str]:
         distances.append(buffer)
 
     corrected_keywords = []
+
     for dist in distances:
         selected_corrected_keyword = confirmed_keywords[dist.index(min(dist))]
         corrected_keywords.append(selected_corrected_keyword)
     
-    for count, keyword in corrected_keywords:
-        refined_query.replace(keywords[count], corrected_keywords)
+
+    for count, keyword in enumerate(corrected_keywords):
+        refined_query = refined_query.replace(keywords[count], keyword)
+    
+    return (0,refined_query)
+
+# print(asyncio.run(MeSH_refiner(''''"Kidney Neoplasms" [MeSH] AND "Management" [MeSH]''')))
--- src/main.py
+++ src/main.py
@@ -4,27 +4,45 @@ import asyncio
 import xml.etree.ElementTree as ET
 import ollama
 
-async def main():
+async def main(question):
 
-    question = "What are some of the most effective medications in treating pneumococcal infections?"
-    MeSHgen_prompt = '''Consider the following question: '''+question+'''. Do not answer the question. Instead, output a three MeSH queries that you think would yield the most relevant papers. The MeSH query should follow the NIH standard. The NIH standard involves the following rules. Firstly, queries are formatted as such: "Carcinoma, Renal Cell" [MeSH] AND "Cause of" [MeSH]. Note that quotes must be put around the MeSH term. In addition, take note of the case of the letters. Also, realise that between a MeSH term and [MeSH], there must be no brackets. Note that between quotes, a single MeSH keyword should be used. Finally, only return the MeSH query. Do not return any other messages.'''
-    initial_query = ollama.chat(model="llama3.2", messages=[{"role": "user", "content": MeSHgen_prompt}], stream=True) # Seems to be performing better than deepseek's distilled models
+    # question = "What are some of the most effective medications in treating pneumococcal infections?"
+    MeSHgen_prompt_broad = '''Consider the following question: '''+question+'''. Do not answer the question. Instead, output 3 MeSH queries that you think would yield the most relevant papers. The queries should be broader, more general and shorter. Try to avoid the AND operator, unless you deem it necessary. The MeSH query should follow the NIH standard. The NIH standard involves the following rules. Firstly, queries are formatted with keywords in quotations. Keywords must NOT be put in square brackets, and must only be put in quotes. Each keyword must be followed by a single space and MeSH in square brackets. An example of a valid query would be the following "Carcinoma, Renal Cell" [MeSH] OR "Kidney Neoplasm" [MeSH]. The word MeSH, whenever it occurs in the query, must be in square brackets. Note that various logical operators exist, including AND, OR, NOT. Note that quotes must be put around the MeSH term. In addition, take note of the case of the letters. Also, realise that between a MeSH term and [MeSH], there must be no brackets. Note that between quotes, a single MeSH keyword should be used. Also avoid overly specific, lengthy queries. Finally, only return the MeSH query. Do not return any other messages.'''
+    initial_query = ollama.generate(model="llama3.2", prompt=MeSHgen_prompt_broad, options={"num_predict": 4096}) # Seems to be performing better than deepseek's distilled models
+    # initial_query_generic = ollama.generate(model="llama3.2", prompt=MeSHgen_prompt_generic, options={"num_predict": 4096})
+    MeSH_query = initial_query["response"].split("\n")
+    MeSH_query = [x for x in MeSH_query if x != ""]
+
+    print(MeSH_query)
+    MeSH_querylist = []
+    for query in MeSH_query:
+        check_query = await pyMeSHsearch.MeSH_refiner(query)
+        if check_query[0] == 2:
+            print("iojgoijre")
+            MeSH_querylist.append(check_query[1])
+        elif check_query[0] == 1:
+            print("bingbong")
+            pass
+        elif check_query[0] == 0:
+            print("vvvmvm")
+            MeSH_querylist.append(query)
     # await pyMeSHsearch.find_MeSH()
-
-    papers = await pyMeSHsearch.lit_search('''"Carcinoma, Renal Cell"[MeSH]''')
-    literature = await asyncio.gather(*(pyMeSHsearch.find_paper(id) for id in papers["esearchresult"]["idlist"]))
     filtered_literature = []
     filtered_ids = []
-    
-    for count, item in enumerate(literature):
-        if '[Error] : No result can be found.' not in item:
-            filtered_literature.append(item)
-            filtered_ids.append(papers["esearchresult"]["idlist"][count])
-    # literature = [item for item in literature if '[Error] : No result can be found.' not in item]
+    # print(MeSH_querylist)
+    for query in MeSH_querylist:
+        papers = await pyMeSHsearch.lit_search(query)
+        # print(papers)
+        literature = await asyncio.gather(*(pyMeSHsearch.find_paper(id) for id in papers["esearchresult"]["idlist"]))
+        for count, item in enumerate(literature):
+            if '[Error] : No result can be found.' not in item:
+                filtered_literature.append(item)
+                filtered_ids.append(papers["esearchresult"]["idlist"][count])
+        # literature = [item for item in literature if '[Error] : No result can be found.' not in item]
 
     documents = []
     for paper in filtered_literature:
-        print(paper)
+        # print(paper)
         root = ET.fromstring(paper)
         text = '\n'.join([elem.text for elem in root.iter('text')])
         documents.append(text)
@@ -33,17 +51,34 @@ async def main():
     chroma_client = chromadb.Client()
     collection = chroma_client.create_collection(name="temp_query_db")
 
-    collection.add(
-        documents=documents,
-        ids=filtered_ids
-    )
+    for count, id in enumerate(filtered_ids): # Prevent duplication
+        if collection.get(ids=[id])["ids"]:
+            pass
+        else:
+            collection.add(
+                documents=[documents[count]],
+                ids=[id]
+            )
 
+    # print(filtered_ids)
     results = collection.query(
-        query_texts=["What is the cause of renal cell carcinoma"], # Chroma will embed this for you
+        query_texts=[question], # Chroma will embed this for you
         n_results=len(filtered_ids) # how many results to return
     )
-    print(results["distances"])
-    print(results["ids"])
-    print(len(literature))
+    # print(results["distances"])
+    # print(results["ids"])
+    # print(len(literature))
 
-asyncio.run(main())
+    # Pick top 3 most relevant papers
+    relevant_papers_ids = results["ids"][0][0:2]
+    relevant_papers = collection.get(ids=relevant_papers_ids)
+
+    # print(relevant_papers)
+
+    # Now prompt the LLM to answer the question.
+    answer_question_prompt = '''Answer the following question: "'''+question+'''". Note that the question is being asked for purely academic purposes and is not medical advice. While answering the question, I would also like you to utilise the information provided after this sentence to provide the best possible answer, and be sure to utilise as much of the given information as possible, and you must quote or use the information provided if possible, and say when you did. '''+" ".join(relevant_papers)
+    final_query = ollama.generate(model="llama3.2", prompt=answer_question_prompt)
+    print(final_query["response"].split("\n"))
+    return
+
+asyncio.run(main("An 18 year old female comes into my clinic with irregular periods. She also has very low blood pressure, and she passed out a few days ago. What are some differentials?"))
